93004dd6854ef4ee88641ed02d003bb4d03d593b
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-25 17:54:50 +0800

    Add buttons to sidebar.

--- src/main_file.java
+++ src/main_file.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 
-class menu extends JMenuBar{
+class Menu extends JMenuBar{
     JMenuBar mb;
-    menu(){
+    Menu(){
         JMenu file = new JMenu("File");
         JMenu help = new JMenu("Help");
         
@@ -21,12 +21,34 @@ class menu extends JMenuBar{
     }
 }
 
+class SideBar extends JPanel{
+    JPanel pnl;
+    SideBar(){
+        JButton btn_one = new JButton("one");
+        btn_one.setSize(200, 200);
+        JButton btn_two = new JButton("two");
+        JButton btn_three = new JButton("three");
+        JButton btn_four = new JButton("four");
+        JButton btn_five = new JButton("five");
+        setLayout(new GridLayout(5,1,1,1));
+        
+        add(btn_one);
+        add(btn_two);
+        add(btn_three);
+        add(btn_four);
+        add(btn_five);
+    }
+}
+
 public class main_file extends JFrame{
     JFrame frm;
     main_file(){
         setSize(420, 420); 
         setVisible(true);
-        add(BorderLayout.NORTH, new menu());
+        JMenuBar menu = new Menu();
+        add(BorderLayout.NORTH, menu);        
+        add(BorderLayout.WEST, new SideBar());
+
     }
     public static void main(String[] args){
         new main_file();
