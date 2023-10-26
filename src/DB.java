ff8bf2a10323f0eb713cf196947fbba667c0b925
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-27 06:44:17 +0800

    add delete function in DB.

--- src/DB.java
+++ src/DB.java
@@ -4,11 +4,26 @@ import java.util.LinkedList;
 
 
 
-public class DB{
+public class DB {
     String url = "jdbc:mysql://localhost:3306/medicalDB";
     String pass = "108996eE@emman"; 
     String username = "root";
 
+    public static void del(int Id) throws Exception{
+        DB db = new DB();
+        String url = db.url;
+        String username = db.username; 
+        String pass = db.pass; 
+ 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+        String query = String.format("DELETE FROM patient_data where patient_id = %d", Id);
+        
+        int rs = st.executeUpdate(query);
+        // System.out.println(query);
+        con.close();
+    }
     public static void insert(String name, String diagnosis, String prescription, String description) throws Exception {
         DB db = new DB();
         String url = db.url;
@@ -22,7 +37,6 @@ public class DB{
         String query = String.format("INSERT INTO patient_data(name, diagnosis, prescription, description) VALUES ('%s', '%s', '%s', '%s')", name, diagnosis, prescription, description);
         System.out.println(query);
         int rs = st.executeUpdate(query);
-        // con.commit();
         con.close();
     }
     public static LinkedList<String> select() throws Exception{
@@ -41,12 +55,15 @@ public class DB{
         while (rs.next()){
             data.add(rs.getInt(1) + " " +rs.getString(2) + " " +rs.getString(3) + " " +rs.getString(4) + " " +rs.getString(5));
         }
-        // System.out.println(data);
+        System.out.println(data);
         con.close();
         return data;
     }
     public static void main(String[] args) throws Exception{
         // insert("someoneNew", "something", "something", "something");
-        // select();
+        select();
+        del(7);
+        select();
+
     }
 }
