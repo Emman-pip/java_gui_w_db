3dec61d2a7e7af90373fadf26075edd4d282a443
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-29 16:29:37 +0800

    New file delete.java, added the gui(no functionality yet).

---
+++ src/delete.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+
+public class delete extends JFrame{
+   JFrame frm;
+   delete(){
+        JPanel pnl = new JPanel();
+        JPanel pnl_buttons = new JPanel();
+
+        JLabel lbl_deleteID = new JLabel("Delete record with ID#:");
+        JTextArea txt_IDNum = new JTextArea();
+        txt_IDNum.setColumns(20);
+        // txt_IDNum.setSize(10,10);
+
+        JButton btn_del = new JButton("Delete");
+        JButton btn_close = new JButton("Close");
+
+
+        // this.add(lbl_deleteID);
+        // this.add(txt_IDNum);
+        this.setTitle("delete records");
+        this.setSize(350,150);
+        // this.pack();
+        this.setLayout(new BorderLayout());
+        pnl_buttons.setLayout(new BorderLayout());
+
+        this.getContentPane().add(pnl, BorderLayout.CENTER);
+        this.getContentPane().add(pnl_buttons, BorderLayout.SOUTH);
+
+        pnl.setBorder(new EmptyBorder(10,10,10,10));
+        // pnl.setLayout(new BorderLayout());
+        // pnl.add(lbl_deleteID, BorderLayout.WEST);
+        // pnl.add(txt_IDNum, BorderLayout.CENTER);
+        pnl.add(lbl_deleteID);
+        pnl.add(txt_IDNum);
+        pnl_buttons.add(btn_del, BorderLayout.CENTER);
+        pnl_buttons.add(btn_close, BorderLayout.SOUTH);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args){
+        new delete();
+    }
+}
