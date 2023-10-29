dd5a4ede922b842346f9c5cb170d56de475fc012
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-29 17:35:05 +0800

    Add update gui(no functionality yet).

--- src/update.java
+++ src/update.java
@@ -2,8 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 
 public class update extends JFrame {
-    update() {
+    JFrame frm;
 
+    update() {
+        JLabel lbl_ID = new JLabel("ID:", JLabel.CENTER);
+        JTextArea txt_ID = new JTextArea();
+        JLabel lbl_name = new JLabel("name:", JLabel.CENTER);
+        JTextArea txt_name = new JTextArea();
+        JLabel lbl_diagnosis = new JLabel("diagnosis:", JLabel.CENTER);
+        JTextArea txt_diagnosis = new JTextArea();
+        JLabel lbl_prescription = new JLabel("prescription:", JLabel.CENTER);
+        JTextArea txt_prescription = new JTextArea();
+        JLabel lbl_description = new JLabel("description:", JLabel.CENTER);
+        JTextArea txt_description = new JTextArea();
+        JButton btn_show = new JButton("Check ID");
+
+        JButton btn_update = new JButton("update");
+
+        this.add(lbl_ID);
+        this.add(txt_ID);
+        this.add(lbl_name);
+        this.add(txt_name);
+        this.add(lbl_diagnosis);
+        this.add(txt_diagnosis);
+        this.add(lbl_prescription);
+        this.add(txt_prescription);
+        this.add(lbl_description);
+        this.add(txt_description);
+        this.add(btn_show);
+        this.add(btn_update);
+
+        this.setLayout(new GridLayout(6, 2));
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("update records");
+        this.setSize(400, 300);
+        this.setVisible(true);
     }
 
     public static void main(String[] args) {
