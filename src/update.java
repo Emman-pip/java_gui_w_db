0127b9c08c86d50795a4bc1028bc046282d1b698
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-29 17:17:50 +0800

    Add update.java

---
+++ src/update.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class update extends JFrame {
+    update() {
+
+    }
+
+    public static void main(String[] args) {
+        new update();
+    }
+}
