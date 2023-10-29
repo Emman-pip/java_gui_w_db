d526344c38e2376f56218ffffa75de4a663808d0
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-29 17:58:56 +0800

    Add functionality to check id button and add additional select function to DB.java for single ID queries.

--- src/DB.java
+++ src/DB.java
@@ -2,79 +2,113 @@ import java.io.*;
 import java.sql.*;
 import java.util.LinkedList;
 
-
-
 public class DB {
     String url = "jdbc:mysql://localhost:3306/medicalDB";
-    String pass = "108996eE@emman"; 
+    String pass = "108996eE@emman";
     String username = "root";
 
-    public static void update(int Id, String name,  String diagnosis, String prescription, String description) throws Exception{
+    public static void update(int Id, String name, String diagnosis, String prescription, String description)
+            throws Exception {
         DB db = new DB();
         String url = db.url;
-        String username = db.username; 
-        String pass = db.pass; 
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, username, pass);
-        Statement st = con.createStatement();
-        
-        String query = String.format("UPDATE patient_data SET name = '%s', diagnosis = '%s', prescription = '%s', description = '%s' WHERE patient_id = %d", name, diagnosis, prescription, description, Id);
-
-        int rs = st.executeUpdate(query);
-        // System.out.println(query);
-        con.close();
-    }
-
-    public static void del(int Id) throws Exception{
-        DB db = new DB();
-        String url = db.url;
-        String username = db.username; 
-        String pass = db.pass; 
- 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, username, pass);
-        Statement st = con.createStatement();
-        String query = String.format("DELETE FROM patient_data where patient_id = %d", Id);
-        
-        int rs = st.executeUpdate(query);
-        // System.out.println(query);
-        con.close();
-    }
-    public static void insert(String name, String diagnosis, String prescription, String description) throws Exception {
-        DB db = new DB();
-        String url = db.url;
-        String username = db.username; 
-        String pass = db.pass; 
- 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, username, pass);
-        Statement st = con.createStatement();
-        
-        String query = String.format("INSERT INTO patient_data(name, diagnosis, prescription, description) VALUES ('%s', '%s', '%s', '%s')", name, diagnosis, prescription, description);
-        System.out.println(query);
-        int rs = st.executeUpdate(query);
-        con.close();
-    }
-    public static LinkedList<String> select() throws Exception{
-        DB db = new DB();
-        String url = db.url;
-        String username = db.username; 
+        String username = db.username;
         String pass = db.pass;
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, username, pass);
         Statement st = con.createStatement();
-        
+
+        String query = String.format(
+                "UPDATE patient_data SET name = '%s', diagnosis = '%s', prescription = '%s', description = '%s' WHERE patient_id = %d",
+                name, diagnosis, prescription, description, Id);
+
+        int rs = st.executeUpdate(query);
+        // System.out.println(query);
+        con.close();
+    }
+
+    public static void del(int Id) throws Exception {
+        DB db = new DB();
+        String url = db.url;
+        String username = db.username;
+        String pass = db.pass;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+        String query = String.format("DELETE FROM patient_data where patient_id = %d", Id);
+
+        int rs = st.executeUpdate(query);
+        // System.out.println(query);
+        con.close();
+    }
+
+    public static void insert(String name, String diagnosis, String prescription, String description) throws Exception {
+        DB db = new DB();
+        String url = db.url;
+        String username = db.username;
+        String pass = db.pass;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+
+        String query = String.format(
+                "INSERT INTO patient_data(name, diagnosis, prescription, description) VALUES ('%s', '%s', '%s', '%s')",
+                name, diagnosis, prescription, description);
+        System.out.println(query);
+        int rs = st.executeUpdate(query);
+        con.close();
+    }
+
+    public static LinkedList<String> select() throws Exception {
+        DB db = new DB();
+        String url = db.url;
+        String username = db.username;
+        String pass = db.pass;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+
         String query = "SELECT * FROM patient_data";
         ResultSet rs = st.executeQuery(query);
         LinkedList<String> data = new LinkedList<String>();
-        while (rs.next()){
-            data.add(rs.getInt(1) + " " +rs.getString(2) + " " +rs.getString(3) + " " +rs.getString(4) + " " +rs.getString(5));
+        while (rs.next()) {
+            data.add(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) + " "
+                    + rs.getString(5));
         }
         System.out.println(data);
         con.close();
         return data;
     }
-    public static void main(String[] args) throws Exception{}
+
+    public static LinkedList<String> selectWithID(String id) throws Exception {
+        DB db = new DB();
+        String url = db.url;
+        String username = db.username;
+        String pass = db.pass;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, username, pass);
+        Statement st = con.createStatement();
+
+        String query = "SELECT * FROM patient_data WHERE patient_id = " + id;
+        ResultSet rs = st.executeQuery(query);
+        LinkedList<String> data = new LinkedList<String>();
+        while (rs.next()) {
+            data.add("" + rs.getInt(1));
+            data.add(rs.getString(2));
+            data.add(rs.getString(3));
+            data.add(rs.getString(4));
+            data.add(rs.getString(5));
+
+        }
+        System.out.println(data);
+        con.close();
+        return data;
+    }
+
+    public static void main(String[] args) throws Exception {
+    }
 }
--- src/update.java
+++ src/update.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 public class update extends JFrame {
     JFrame frm;
@@ -16,6 +19,22 @@ public class update extends JFrame {
         JLabel lbl_description = new JLabel("description:", JLabel.CENTER);
         JTextArea txt_description = new JTextArea();
         JButton btn_show = new JButton("Check ID");
+
+        btn_show.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    LinkedList<String> fetchedData = new DB().selectWithID(txt_ID.getText());
+                    System.out.println(fetchedData);
+                    txt_name.setText(fetchedData.get(1));
+                    txt_diagnosis.setText(fetchedData.get(2));
+                    txt_prescription.setText(fetchedData.get(3));
+                    txt_description.setText(fetchedData.get(4));
+
+                } catch (Exception err) {
+                    System.out.println(err);
+                }
+            }
+        });
 
         JButton btn_update = new JButton("update");
 
